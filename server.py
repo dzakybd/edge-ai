a6f1d7ea743115a80adc8cd2fde887bae2a7c8ef
Dzaky <dzakybd@gmail.com>
2020-02-12 02:13:04 +0900

    hjg

--- server.py
+++ server.py
@@ -6,10 +6,10 @@ port = 4000
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', port))
 
-print("Server Started")
 while True:
     data, addr = s.recvfrom(1024)
-    data = json.loads(data.decode())
-    # print(str(addr), data.get("msg"))
+    # print(str(addr))
     message = json.dumps({"msg": 'success'})
     s.sendto(message.encode(), addr)
+
+
